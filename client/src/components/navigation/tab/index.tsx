7971916ed6089e51ead3a60b18038e86d8821e0b
danielfamiyeh <daniel.kwaku.famiyeh@gmail.com>
2023-01-03 14:04:08 +0000

    Add icons to tab navigation

--- client/src/components/navigation/tab/index.tsx
+++ client/src/components/navigation/tab/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Icon from 'react-native-vector-icons/FontAwesome';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import FriendsView from '../../../views/friends';
@@ -6,11 +7,36 @@ import MessageFlow from '../stack/flows/message';
 
 const Tab = createBottomTabNavigator();
 
+const screens = [
+  { name: 'Friends', Component: FriendsView, iconName: 'users' },
+  {
+    name: 'Messages',
+    Component: MessageFlow,
+    iconName: 'wechat',
+    options: {
+      headerShown: false,
+    },
+  },
+];
+
 const TabNavigator = () => {
   return (
     <Tab.Navigator>
-      <Tab.Screen name="Friends" component={FriendsView} />
-      <Tab.Screen name="Messages" component={MessageFlow} />
+      {screens.map(({ name, Component, iconName, options = {} }) => {
+        return (
+          <Tab.Screen
+            key={name}
+            name={name}
+            component={Component}
+            options={() => ({
+              tabBarIcon: ({ color }) => (
+                <Icon size={24} name={iconName} color={color} />
+              ),
+              ...options,
+            })}
+          />
+        );
+      })}
     </Tab.Navigator>
   );
 };
