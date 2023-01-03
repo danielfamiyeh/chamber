2f82e3a9937d1e0b77e2d5ce3a7e2dfa41123b0d
danielfamiyeh <daniel.kwaku.famiyeh@gmail.com>
2023-01-03 01:15:12 +0000

    Add stylesheet to main file

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
-import { SafeAreaView } from 'react-native';
+import { StyleSheet, SafeAreaView } from 'react-native';
 
 import Navigator from './components/navigation';
 
 const App = () => {
   return (
-    <SafeAreaView style={{ flex: 1 }}>
+    <SafeAreaView style={styles.container}>
       <Navigator />
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
 
 export default App;
