6389213e518eb09871ffb7cc92a560476bbc6ce6
danielfamiyeh <daniel.kwaku.famiyeh@gmail.com>
2023-01-04 16:36:09 +0000

    Add registration to user service

--- user-service/src/index.ts
+++ user-service/src/index.ts
@@ -1,18 +1,21 @@
 require('dotenv').config();
 
 import cors from 'cors';
+import axios from 'axios';
 import express from 'express';
 import bodyParser from 'body-parser';
 
 import { log } from './utils/logger';
 import { router } from './routes';
 
-const PORT = Number(process.env.PORT);
-
 export default express()
   .use(cors())
   .use(bodyParser.json())
   .use('/api', router)
-  .listen(PORT, () =>
-    log('info', `Chamber (User Service) listening on port: ${PORT}`)
-  );
+  .listen(process.env.PORT, () => {
+    log(
+      'info',
+      `Chamber (User Service) listening on port: ${process.env.PORT}`
+    );
+    axios.put(`${process.env.REGISTRY_URL}/user`);
+  });
