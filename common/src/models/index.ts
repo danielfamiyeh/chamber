d6eea610d19b65dcf77b62c6b4319336b22bc33a
danielfamiyeh <daniel.kwaku.famiyeh@gmail.com>
2023-01-07 10:25:30 +0000

    Add Post and Content models to named export

--- common/src/models/index.ts
+++ common/src/models/index.ts
@@ -1,11 +1,15 @@
 import { FriendRequest } from './friend-request';
 import { Message } from './message';
+import { Content } from './content';
 import { Chat } from './chat';
 import { User } from './user';
+import { Post } from './post';
 
 export const models = {
-  FriendRequest,
-  User,
   Chat,
+  Post,
+  User,
+  Content,
   Message,
+  FriendRequest,
 };
