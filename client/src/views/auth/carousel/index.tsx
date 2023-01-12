0ed414f8cf139893e70ed37a7939ceb868662ac8
danielfamiyeh <daniel.kwaku.famiyeh@gmail.com>
2023-01-12 07:49:00 +0000

    Add controls to carousel

--- client/src/views/auth/carousel/index.tsx
+++ client/src/views/auth/carousel/index.tsx
@@ -8,17 +8,36 @@ import { slides } from './utils/constants';
 import styles from './styles';
 
 const Carousel = () => {
-  const [state, _setState] = React.useState({
+  const [state, setState] = React.useState({
     slideIdx: 0,
   });
-
-  const setState = (ns: object) =>
-    _setState((_state) => ({ ..._state, ...ns }));
 
   const currentSlide = React.useMemo(
     () => slides[state.slideIdx],
     [state.slideIdx]
   );
+
+  const onPressNext = () =>
+    setState((_state) => {
+      const ns = { ..._state };
+
+      if (_state.slideIdx < slides.length - 1) {
+        ns.slideIdx += 1;
+      }
+
+      return ns;
+    });
+
+  const onPressPrev = () =>
+    setState((_state) => {
+      const ns = { ..._state };
+
+      if (_state.slideIdx > 0) {
+        ns.slideIdx -= 1;
+      }
+
+      return ns;
+    });
 
   return (
     <View style={styles.container}>
@@ -28,10 +47,18 @@ const Carousel = () => {
         description={currentSlide.description}
       />
       <View style={styles.ctaContainer}>
-        <Button disabled style={styles.controlButton}>
+        <Button
+          disabled={state.slideIdx <= 0}
+          style={styles.controlButton}
+          onPress={onPressPrev}
+        >
           <Text>Prev.</Text>
         </Button>
-        <Button style={styles.controlButton}>
+        <Button
+          disabled={state.slideIdx >= slides.length - 1}
+          style={styles.controlButton}
+          onPress={onPressNext}
+        >
           <Text>Next.</Text>
         </Button>
       </View>
