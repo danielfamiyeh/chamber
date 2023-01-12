8dd2d39c9b3d99c7baaf743fb166528580f19525
danielfamiyeh <daniel.kwaku.famiyeh@gmail.com>
2023-01-12 10:03:02 +0000

    Improve input field styling and rendering

--- client/src/components/input/form/field/FormField.tsx
+++ client/src/components/input/form/field/FormField.tsx
@@ -6,16 +6,36 @@ import { TextInput } from 'react-native-gesture-handler';
 import styles from './FormField.styles';
 
 const FormField = (props: FormFieldProps) => {
+  const InputField = React.useMemo(
+    () => () => {
+      switch (props.value.constructor.name) {
+        case 'String':
+        case 'Number':
+          return (
+            <TextInput
+              keyboardType={
+                props.value.constructor.name === 'Number'
+                  ? 'numeric'
+                  : 'default'
+              }
+              style={styles.textInput}
+              value={props.value.toString()}
+              onChangeText={(value: string) =>
+                props.onChange(props.value.constructor(value))
+              }
+            />
+          );
+        default:
+          return null;
+      }
+    },
+    [props]
+  );
+
   return (
     <View style={styles.container}>
       <Text style={styles.label}>{startCase(props.label)}</Text>
-      <TextInput
-        style={styles.inputContainer}
-        value={props.value.toString()}
-        onChangeText={(value: string) =>
-          props.onChange(props.value.constructor(value))
-        }
-      />
+      <InputField />
     </View>
   );
 };
