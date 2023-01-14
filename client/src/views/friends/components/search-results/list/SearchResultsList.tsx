415dafe1753532189763aa4bef4991893ac3c1be
danielfamiyeh <daniel.kwaku.famiyeh@gmail.com>
2023-01-14 07:32:28 +0000

    Basic user search

--- client/src/views/friends/components/search-results/list/SearchResultsList.tsx
+++ client/src/views/friends/components/search-results/list/SearchResultsList.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Text, View } from 'react-native';
-import { User } from '@danielfamiyeh/chamber-common';
+import { FlatList } from 'react-native-gesture-handler';
 import Icon from 'react-native-vector-icons/FontAwesome5';
 
 import Button from '../../../../../components/input/button/Button';
 
+import SearchResultsListItem, {
+  SearchResult,
+} from '../list-item/SearchResultsListItem';
 import styles from './SearchResultsList.styles';
 
 const SearchResultList = (props: SearchResultListProps) => {
@@ -21,14 +24,19 @@ const SearchResultList = (props: SearchResultListProps) => {
       </View>
     );
   } else if (props.results.length) {
-    return null;
+    return (
+      <FlatList
+        renderItem={({ item }) => <SearchResultsListItem {...item} />}
+        data={props.results}
+        contentContainerStyle={styles.searchResultsContainer}
+      />
+    );
   } else {
     return (
       <View style={styles.listEmptyContainer}>
         <Icon name="sad-cry" style={styles.listEmptyIcon} size={96} />
         <Text style={styles.listEmptyText}>How sad.</Text>
         <Text style={styles.listEmptyText}>You have no friends!</Text>
-
         <Button style={styles.searchButton} onPress={props.onSearchAgain}>
           <Text style={styles.searchAgainButtonText}>Find Some</Text>
         </Button>
@@ -39,7 +47,7 @@ const SearchResultList = (props: SearchResultListProps) => {
 
 export interface SearchResultListProps {
   onSearchAgain: () => void;
-  results: { user: User; friendsSince: Date }[];
+  results: SearchResult[];
   hasSearched: boolean;
 }
 
